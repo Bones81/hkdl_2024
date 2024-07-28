3a71ff6969d226fee3113e3e0c3e42a4bedc963f
Nathan Freeman <nathanlfreeman@gmail.com>
2024-07-28 13:00:54 -0400

    tweak vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./hkdl_2024",
+  base: "./hkdl_2024/",
   plugins: [react()],
 })

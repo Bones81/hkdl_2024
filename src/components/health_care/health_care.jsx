8e12e907be8837178f3a0635570467e358c0ce70
Nathan Freeman <nathanlfreeman@gmail.com>
2024-07-29 00:15:09 -0400

    update health section

--- src/components/health_care/health_care.jsx
+++ src/components/health_care/health_care.jsx
@@ -4,13 +4,16 @@ const Health = () => {
         <h2 className="section-title">Health Care and Insurance in HK</h2>
         <h4 class="special-note">
           Our Disney-provided health insurance is supposed to be pretty high
-          quality. I have not personally ever used it. I know one previous
+          quality. I know one previous
           caroler had to get a root canal done in HK. And another had multiple
           outpatient visits to local hospitals for rough illnesses during a
-          contract. If anyone is able to offer more information on the
-          experience of using our health insurance in HK, let me know, and we'll
-          add it to this section.
+          contract.
         </h4>
+        <p>
+          In 2023, some pretty rough illnesses spread through the dancers and vocalists at HKDL. Unfortunately, despite taking many precautions, pretty much all of us dealt with some version of the illness, and a few of us did have to take sick days off of work.
+        </p>
+        <p>A doctor's note is required to clear your sick days. As a result, even though you may feel pretty awful, you need to visit a local clinic with a doctor who takes our insurance. Fortunately there were a couple of clinics about a 10-minute walk from our HPRC housing. I will try to post their info here so that we can be aware of them should we need them this year.</p>
+        <p>The doctor's visits were ultimately very efficient, with the doctor asking a few questions to determine the nature of the illness, followed by issuing the doctor's note and several different meds targeting any and all symptoms. With our insurance, there was HKD$0 for all services.</p>
         <p>
           As part of our first couple of weeks at Disney, we will eventually
           receive a packet with our health insurance benefits and information.
